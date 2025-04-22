d170ac0953127e3616060fff8864f12af4675b71
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-04-22 18:26:30 +0530

    initial cmmit for firebase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Home from './pages/home.jsx';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDCYgzuDiESSqecUHFNeaYK5xEGDqraMcg",
+  authDomain: "demofirebase-exe.firebaseapp.com",
+  projectId: "demofirebase-exe",
+  storageBucket: "demofirebase-exe.firebasestorage.app",
+  messagingSenderId: "58026073944",
+  appId: "1:58026073944:web:20adf808c3b378608b8f2d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestorefs = getFirestore(app);
+
